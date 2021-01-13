4249f40152c0c62f8a576210fd224009c66af289
James Parker <James.Parker10@gmail.com>
2021-01-13 10:37:36 -0600

    Initial commit

---
+++ cap_touch_osc_test.py
@@ -0,0 +1,43 @@
+import time
+import board
+import busio
+import adafruit_mpr121
+
+import argparse
+from pythonosc import udp_client
+
+#set up argparser for OSC sending
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1")
+  parser.add_argument("--port", type=int, default=5005)
+  args = parser.parse_args()
+
+  client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+#set up the capacitive touch board
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mpr121 = adafruit_mpr121.MPR121(i2c)
+
+
+while True:
+    # Loop through all 12 inputs (0-11).
+    for i in range(12):
+        if mpr121[i].value:
+            current_touched = i
+
+            if current_touched != last_touched:
+                print(f'{current_touched} touched!')
+                client.send_message("/test", [i, 'touched'])
+
+            last_touched = current_touched
+
+    if not mpr121[current_touched].value and current_touched == last_touched:
+        print(f'{current_touched} released!')
+        client.send_message("/test", [i, 'released'])
+
+        current_touched = 10
+        last_touched = 11
+
+    time.sleep(0.1)
---
+++ veml_osc_test.py
@@ -0,0 +1,34 @@
+import time
+import board
+import busio
+import adafruit_veml7700
+import argparse
+import time
+from pythonosc import udp_client
+
+i2c = busio.I2C(board.SCL, board.SDA)
+veml7700 = adafruit_veml.VEML7700(i2c)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", default = "127.0.0.1",
+        help="The ip of the OSC server")
+    parser.add_argument("--port", type=int, default=5005,
+        help="The port the OSC server is listening on")
+    args = parser.parse_args()
+
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+    for x in range(10):
+        client.send_message("/test", random.random())
+        print("message sent")
+        time.sleep(1)
+
+
+
+while True:
+    print("Ambient light:", veml7700.light)
+    print("Lux:", veml7700.lux)
+    client.send_message("/veml7700", [veml7700.light, veml7700.lux])
+
+    time.sleep(0.1)
